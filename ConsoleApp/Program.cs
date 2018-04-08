596d369a65559ae73752800f1452f61f3abdf4d5
aldaniluk <allldaniluk@gmail.com>
2018-04-08 19:55:48 +0300

    Added Simulated Annealing algorithm

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,36 +11,44 @@ namespace ConsoleApp
             var inf = int.MaxValue;
             var arr = new int[,]
             {
-                {inf,2,5,1,7,1,5,2,6,5},
-                {2,inf,3,2,9,1,9,5,7,2},
-                {5,3,inf,8,6,2,8,1,3,4},
-                {1,2,8,inf,3,4,1,2,4,1},
-                {7,9,6,3,inf,7,5,7,4,6},
-                {1,1,2,4,7,inf,4,9,1,3},
-                {5,9,8,1,5,4,inf,1,7,8},
-                {2,5,1,2,7,9,1,inf,4,4},
-                {6,7,3,4,4,1,7,4,inf,9},
-                {5,2,4,1,6,3,8,4,9,inf}
+                {inf,21,53,11,72,13,54,25,66,57},
+                {21,inf,32,28,97,16,95,54,73,23},
+                {53,32,inf,86,65,24,83,12,31,40},
+                {11,28,86,inf,31,42,13,24,45,16},
+                {72,97,65,31,inf,78,57,76,45,65},
+                {13,16,24,42,78,inf,4,95,15,35},
+                {54,95,83,13,57,4,inf,11,72,83},
+                {25,54,12,24,76,95,11,inf,44,44},
+                {66,73,31,45,45,15,72,44,inf,59},
+                {57,23,40,16,65,35,83,44,95,inf}
             };
             var graph = new Graph(arr);
 
-            Console.WriteLine($"{"algorithm name",25} {"iterations",15} {"result",10} {"time (ms)",15}");
+            Console.WriteLine($"{"algorithm name",40} {"iterations",15} {"result",10} {"time (ms)",15}");
 
             ReturnData bruteForceSearch = BruteForceSearch.Search(graph);
-            Console.WriteLine($"{bruteForceSearch.AlgorithmName,25} {bruteForceSearch.Iterations,15} " +
+            Console.WriteLine($"{bruteForceSearch.AlgorithmName,40} {bruteForceSearch.Iterations,15} " +
                 $"{bruteForceSearch.Result,10} {bruteForceSearch.TimeInMs,15}");
 
             ReturnData greedyAlgorithm = GreedyAlgorithm.Search(graph);
-            Console.WriteLine($"{greedyAlgorithm.AlgorithmName,25} {greedyAlgorithm.Iterations,15} " +
+            Console.WriteLine($"{greedyAlgorithm.AlgorithmName,40} {greedyAlgorithm.Iterations,15} " +
                 $"{greedyAlgorithm.Result,10} {greedyAlgorithm.TimeInMs,15}");
 
             ReturnData branchAndBoundAlgorithm = BranchAndBoundAlgorithm.Search(graph);
-            Console.WriteLine($"{branchAndBoundAlgorithm.AlgorithmName,25} {branchAndBoundAlgorithm.Iterations,15} " +
+            Console.WriteLine($"{branchAndBoundAlgorithm.AlgorithmName,40} {branchAndBoundAlgorithm.Iterations,15} " +
                 $"{branchAndBoundAlgorithm.Result,10} {branchAndBoundAlgorithm.TimeInMs,15}");
 
             ReturnData localSearch2Algorithm = LocalSearch2.Search(graph);
-            Console.WriteLine($"{localSearch2Algorithm.AlgorithmName,25} {localSearch2Algorithm.Iterations,15} " +
+            Console.WriteLine($"{localSearch2Algorithm.AlgorithmName,40} {localSearch2Algorithm.Iterations,15} " +
                $"{localSearch2Algorithm.Result,10} {localSearch2Algorithm.TimeInMs,15}");
+
+            ReturnData simulatedAnnealingAlgorithm_lin = SimulatedAnnealing.Search(graph, SimulatedAnnealing.GetNewTemperature_Linear);
+            Console.WriteLine($"{simulatedAnnealingAlgorithm_lin.AlgorithmName + "(lin)",40} {simulatedAnnealingAlgorithm_lin.Iterations,15} " +
+               $"{simulatedAnnealingAlgorithm_lin.Result,10} {simulatedAnnealingAlgorithm_lin.TimeInMs,15}");
+
+            ReturnData simulatedAnnealingAlgorithm_pow = SimulatedAnnealing.Search(graph, SimulatedAnnealing.GetNewTemperature_Power);
+            Console.WriteLine($"{simulatedAnnealingAlgorithm_pow.AlgorithmName + "(pow)",40} {simulatedAnnealingAlgorithm_pow.Iterations,15} " +
+               $"{simulatedAnnealingAlgorithm_pow.Result,10} {simulatedAnnealingAlgorithm_pow.TimeInMs,15}");
         }
     }
 }
---
+++ Logic/Algorithms/SimulatedAnnealing.cs
@@ -0,0 +1,133 @@
+﻿using Logic.Graphs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Logic.Algorithms
+{
+    public static class SimulatedAnnealing
+    {
+        public static double INITIAL_TEMPERATURE { get; set; } = 10;
+        public static double MIN_TEMPERATURE { get; set; } = 0.0000001;
+
+        public static ReturnData Search(Graph graph, Func<double, int, double> getTemperature)
+        {
+            var startTime = DateTime.Now;
+            var iterations = 0;
+
+            List<int> route = GetRoute(graph, ref iterations, getTemperature);
+
+            int result = GetRouteValue(route, graph);
+
+            var endTime = DateTime.Now;
+
+            return new ReturnData("Simulated Annealing Algorithm", result, iterations, (endTime - startTime).Milliseconds);
+        }
+
+        #region GetTemperature methods
+        public static double GetNewTemperature_Linear(double currentTemperature, int iterations)
+        {
+            return currentTemperature * 0.8;
+        }
+
+        public static double GetNewTemperature_Power(double currentTemperature, int iterations)
+        {
+            return currentTemperature / (iterations * iterations);
+        }
+        #endregion
+
+        private static List<int> GetRoute(Graph graph, ref int iterations, Func<double, int, double> getTemperature)
+        {
+            List<int> currentRoute = GetDefaultRoute(graph);
+            int currentRouteValue = GetRouteValue(currentRoute, graph);
+            double currentTemperature = INITIAL_TEMPERATURE;
+
+            while (currentTemperature > MIN_TEMPERATURE)
+            {
+                List<int> possibleRoute = GetPossibleRoute(currentRoute);
+                int possibleRouteValue = GetRouteValue(possibleRoute, graph);
+
+                if (possibleRouteValue < currentRouteValue ||
+                    GoodProbability(GetProbability(possibleRouteValue - currentRouteValue, currentTemperature)))
+                {
+                    currentRoute = possibleRoute;
+                    currentRouteValue = possibleRouteValue;
+                }
+
+                currentTemperature = getTemperature(currentTemperature, ++iterations);
+            }
+
+            return currentRoute;
+        }
+
+        private static double GetProbability(int deltaRouteValue, double temperature)
+        {
+            var probability = Math.Exp(-deltaRouteValue / temperature);
+            
+            return probability;
+        }
+
+        private static bool GoodProbability(double probability)
+        {
+            double randValue = GetRandom(isInt: false);
+
+            return randValue <= probability;
+        }
+
+        private static List<int> GetDefaultRoute(Graph graph)
+        {
+            var route = new List<int>();
+            for (int i = 0; i < graph.Size - 1; i++)
+            {
+                route.Add(i);
+            }
+
+            return route;
+        }
+
+        private static List<int> GetPossibleRoute(List<int> route)
+        {
+            List<int> possibleRoute = route.Select(pr => pr).ToList();
+
+            int firstIndex = GetRandom(0, route.Count - 1, isInt: true);
+            int secondIndex = GetRandom(0, route.Count - 1, isInt: true);
+
+            possibleRoute[firstIndex] = route[secondIndex];
+            possibleRoute[secondIndex] = route[firstIndex];
+
+            return possibleRoute;
+        }
+
+        private static int GetRouteValue(List<int> route, Graph graph)
+        {
+            var result = 0;
+
+            for (int i = 0; i < route.Count - 1; i++)
+            {
+                result += graph.Matrix[route[i], route[i + 1]];
+            }
+
+            result += graph.Matrix[route.Last(), route.First()];
+
+            return result;
+        }
+
+        //REAL random
+        private static readonly Random random = new Random();
+        private static readonly object syncLock = new object();
+        public static dynamic GetRandom(int min = 0, int max = 1, bool isInt = true)
+        {
+            lock (syncLock)
+            {
+                if (isInt)
+                {
+                    return random.Next(min, max);
+                }
+                else
+                {
+                    return random.NextDouble();
+                }
+            }
+        }
+    }
+}
